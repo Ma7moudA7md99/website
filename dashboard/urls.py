a7817823f4642e2129f56f3f710112e283c6724b
Mahmoud Ahmed <wa7wa776@gmail.com>
2024-02-17 20:06:57 +0200

    dashboard update

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('dash', views.dashboard, name='dash'),
   path('update doctor/<int:doctor_id>/', views.update_doctor, name='update doctor'),
-  path('add doctor', views.add_doctor, name='add doctor')
+  path('add doctor', views.add_doctor, name='add doctor'),
+  path('delete/<int:id_delete>/', views.delete_, name='delete')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Doctors
+from userProfile.models import profile
 from .forms import DoctorForm
 
 # Create your views here.
 # function to render dashboard page
 def dashboard(request):
     doctors = Doctors.objects.all()
-    return render(request, 'dashboard.html', {'doctors': doctors})
+    users = User.objects.all()
+    return render(request, 'dashboard.html', {'doctors': doctors, 'users': users})
 
 
 def update_doctor(request, doctor_id):
@@ -39,3 +42,12 @@ def add_doctor(request):
                 form.save()
                 return redirect('dash') 
     return render(request, 'update.html', {'form': form, 'btn_title': 'Add new doctor'})
+
+def delete_(request, id_delete):
+    if Doctors.objects.filter(id=id_delete).exists():
+        doctor = Doctors.objects.get(id= id_delete)
+        doctor.delete()
+    else:
+        user = User.objects.get(id=id_delete)
+        user.delete()
+    return redirect('dash')
